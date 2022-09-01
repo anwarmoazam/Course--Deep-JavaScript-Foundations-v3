f8a878de5a8a0d94d8d25d5a660d5bb6ae950b94
anwarmoazam <anwar.palimarwar@gmail.com>
2022-09-01 23:00:15 +0530

    functions

--- strict.js
+++ strict.js
@@ -1,18 +1,30 @@
-"use strict";
+// "use strict";
 
 var teacher = "Kyle";
-console.log(teacher);
+// console.log(teacher);
 // console.log(topic);
 function otherClass(){
     teacher = "Suzy";
     topic = "React";
-    console.log("Welcome!");
+    // console.log("Welcome!");
 }
 
-console.log(teacher);
+// console.log(teacher);
 // console.log(topic);
 
 otherClass();
 
-console.log(teacher);
-console.log(topic);
+// console.log(teacher);
+// console.log(topic);
+
+function teacher1(){
+    console.log("Hello teacher");
+}
+
+var myTeacher = function anotherTeacher(){
+    console.log(anotherTeacher);
+}
+
+console.log(teacher1);
+console.log(myTeacher);
+console.log(anotherTeacher);
